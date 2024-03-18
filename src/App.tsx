798eb1ae0fbb279fa3e6055dfc5c7b48407f373f
mano <mano.balakrishnan@freshworks.com>
2024-03-18 21:04:38 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React ghs
+          Learn React g
         </a>
       </header>
     </div>
